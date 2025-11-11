718e973be7a9f8cbaa454cc0465342353e312ec4
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2025-11-11 16:33:28 +0000

    fix: corrected API gateway configuration for CORS

--- iac/terraform/api_gateway.tf
+++ iac/terraform/api_gateway.tf
@@ -44,6 +44,52 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.scanner_function.invoke_arn
 }
 
+# 4b. Mock integration for OPTIONS method (CORS preflight)
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.scanner_api.id
+  resource_id = aws_api_gateway_resource.scan_resource.id
+  http_method = aws_api_gateway_method.scan_options_method.http_method
+  type        = "MOCK"
+}
+
+# 4c. Method response for OPTIONS
+resource "aws_api_gateway_method_response" "options_200_response" {
+  rest_api_id = aws_api_gateway_rest_api.scanner_api.id
+  resource_id = aws_api_gateway_resource.scan_resource.id
+  http_method = aws_api_gateway_method.scan_options_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+# 4d. Integration response for OPTIONS (setting the actual header values)
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.scanner_api.id
+  resource_id = aws_api_gateway_resource.scan_resource.id
+  http_method = aws_api_gateway_method.scan_options_method.http_method
+  status_code = aws_api_gateway_method_response.options_200_response.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.options_integration]
+}
+
 # 5. Deployment
 resource "aws_api_gateway_deployment" "scanner_deployment" {
   rest_api_id = aws_api_gateway_rest_api.scanner_api.id
@@ -54,6 +100,9 @@ resource "aws_api_gateway_deployment" "scanner_deployment" {
       aws_api_gateway_method.scan_post_method.id,
       aws_api_gateway_method.scan_options_method.id,
       aws_api_gateway_integration.lambda_integration.id,
+      aws_api_gateway_integration.options_integration.id,
+      aws_api_gateway_method_response.options_200_response.id,
+      aws_api_gateway_integration_response.options_integration_response.id,
     ]))
   }
 
